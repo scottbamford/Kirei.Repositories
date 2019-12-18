8f2ad921c1d1074d626a06b2f8b77a131e132a00
Scott Bamford <scott@ambidect.company>
2019-12-18 16:11:54 +0000

    1. When registering repositories, make sure to register against both IRepository<Model> and IRepository<Model,PrimaryKey>.

--- Kirei.Repositories/ServiceCollectionExtensions.cs
+++ Kirei.Repositories/ServiceCollectionExtensions.cs
@@ -49,8 +49,8 @@ namespace Microsoft.Extensions.DependencyInjection
             where Repository : class, IRepository<Model>
         {
             services.AddModelConverter();
-            
-            services.AddTransient<IRepository<Model>, Repository>();
+
+            AddAllRepositoryInterfaces(services, typeof(Model), typeof(Repository));
 
             return services;
         }
@@ -67,7 +67,7 @@ namespace Microsoft.Extensions.DependencyInjection
             where RepositoryEvents : class, IRepositoryEvents<Model>
         {
             services.AddSingleton<IRepositoryEvents<Model>, RepositoryEvents>();
-            
+
             return services;
         }
 
@@ -92,7 +92,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             var repositoryType = map(request);
             if (repositoryType != null) {
-                services.AddTransient(typeof(IRepository<Model>), repositoryType);
+                AddAllRepositoryInterfaces(services, typeof(IRepository<Model>), repositoryType);
             }
 
             return services;
@@ -106,17 +106,24 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="theNamespace"></param>
         /// <param name="map"></param>
         /// <returns></returns>
+        [Obsolete("This method will be removed in a future release.  Please use AddRepositoryForAssembly passing the namespace.")]
         public static IServiceCollection AddRepositoriesForAssemblyNamespace(this IServiceCollection services, Assembly assembly, string theNamespace, Func<ModelRepositoryMapRequest, Type> map)
         {
-            return AddRepositoriesForAssembly(services, assembly, (request) =>
-            {
-                if (request.ModelType.Namespace != theNamespace) {
-                    return null;
-                }
+            return AddRepositoriesForAssembly(services, assembly, theNamespace, map);
 
-                return map(request);
-            });
+        }
 
+        /// <summary>
+        /// Add repositories for each model class in <paramref name="assembly"/> under <paramref name="theNamespace"/> using <paramref name="map"/> and register any IRepositoryEvents&lt;&gt; from <paramref name="assembly"/>.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="assembly"></param>
+        /// <param name="theNamespace"></param>
+        /// <param name="map"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddRepositoriesForAssembly(this IServiceCollection services, Assembly assembly, Func<ModelRepositoryMapRequest, Type> map)
+        {
+            return AddRepositoriesForAssembly(services, assembly, inNamespace: null, map);
         }
 
         /// <summary>
@@ -126,13 +133,18 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="assembly"></param>
         /// <param name="map"></param>
         /// <returns></returns>
-        public static IServiceCollection AddRepositoriesForAssembly(this IServiceCollection services, Assembly assembly, Func<ModelRepositoryMapRequest, Type> map)
+        public static IServiceCollection AddRepositoriesForAssembly(this IServiceCollection services, Assembly assembly, string inNamespace, Func<ModelRepositoryMapRequest, Type> map)
         {
             services.AddModelConverter();
 
             var types = assembly.GetExportedTypes();
             foreach (var modelType in types) {
-                if (!modelType.IsClass) {
+                if (!modelType.IsClass || modelType.IsAbstract) {
+                    continue;
+                }
+
+                // Restrict by the namespace (if its been specified).
+                if (!String.IsNullOrEmpty(inNamespace) && modelType.Namespace != inNamespace) {
                     continue;
                 }
 
@@ -165,11 +177,56 @@ namespace Microsoft.Extensions.DependencyInjection
 
                 // If we got a repository to map to, add it to the services collection.
                 if (repositoryType != null) {
-                    services.AddTransient(typeof(IRepository<>).MakeGenericType(modelType), repositoryType);
+                    AddAllRepositoryInterfaces(services, modelType, repositoryType);
                 }
             }
 
             return services;
+        }
+
+        /// <summary>
+        /// Add all repository resolutions as servies.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="modelType"></param>
+        /// <param name="repositoryType"></param>
+        private static void AddAllRepositoryInterfaces(IServiceCollection services, Type modelType, Type repositoryType)
+        {
+            // Add the normal resolution.
+            services.AddScoped(typeof(IRepository<>).MakeGenericType(modelType), repositoryType);
+
+            // If the Key property can be found on the model, also register the repository to be resolved with the PrimaryKey type specified too
+            // as some code will want to be built generically around the PrimaryKey type and will want to use this version directly.
+            var keyProperty = GetPrimaryKeyPropertyFromModel(modelType);
+            if (keyProperty != null) {
+                services.AddScoped(typeof(IRepository<,>).MakeGenericType(modelType, keyProperty.PropertyType), repositoryType);
+            }
+        }
+
+        /// <summary>
+        /// Returns the primary key property from <paramref name="model"/> by finding a field matching the primary key of the DbModel type.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        private static System.Reflection.PropertyInfo GetPrimaryKeyPropertyFromModel(Type modelType)
+        {
+            // Lookup the key.
+            var keyProperty = modelType
+                .GetProperties()
+                .FirstOrDefault(item => item.GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.KeyAttribute), inherit: true).Any());
+            if (keyProperty == null) {
+                keyProperty = modelType
+                    .GetProperties()
+                    .FirstOrDefault(item => item.Name == "Id");
+                if (keyProperty == null) {
+                    keyProperty = modelType
+                        .GetProperties()
+                        .FirstOrDefault(item => item.Name == $"{modelType.Name}Id");
+
+                }
+            }
+
+            return keyProperty;
         }
     }
 }
