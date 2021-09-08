0d1bbfaef31f6cb82d55a1248730a2087cc86ab2
Scott Bamford <scott@ambidect.com>
2021-09-08 14:06:02 +0100

    Add InputModelDictionary and InputModelDictionaryObjectGraphType as a new API that supports the latest versions of GraphQL.NET to replace the previous API of GetArgumentAsDynamicDictionary which no longer works with GraphQL.NET's new ParseDictionary() API.

---
+++ Kirei.Repositories.GraphQL/InputModelDictionary/InputModelDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kirei.Repositories.GraphQL
+{
+    /// <summary>
+    /// Class that exposes the parsed model of TModel, the case insensitive dictioary of changes, and the raw Dictionary received as an input from
+    /// a GraphQL.
+    /// 
+    /// To use either include this code in your InputObjectGraphType&lt;TModel&gt; subclass:
+    /// 
+    /// public override object ParseDictionary(IDictionary<string, object> value)
+    /// {
+    ///     var model = (TModel)base.ParseDictionary(value);
+    ///     return new InputModelDictionary<TModel>(model, value);
+    /// }
+    /// 
+    /// Or inherit from InputModelDictionaryObjectGraphType&lt;TModel&gt; to have it done for you automatically.
+    /// 
+    /// To use:
+    ///     var input = context.GetArgument<InputModelDictionary<Models.QuestionTag>>("model");
+    ///     input["FieldName"] // Use the input as a insenstive dictionary (CaseInsenstiveDynamicDictionary).
+    ///     input.GetModel() // Returns the model.
+    ///     input.GetRawDictionary() // Returns the raw dictionary.
+    ///     
+    /// You can also cast to get the model:
+    ///     var model = (TModel)input;
+    /// </summary>
+    public class InputModelDictionary<TModel> : CaseInsenstiveDynamicDictionary
+    {
+        private TModel _model;
+        private IDictionary<string, object> _rawDictionary;
+
+        public InputModelDictionary(TModel model, IDictionary<string, object> dictionary)
+            : base(dictionary)
+        {
+            _model = model;
+            _rawDictionary = dictionary;
+        }
+
+        /// <summary>
+        /// Returns the raw dictionary received by the GraphQL query (contents will mirror those in Changes but without any case insensitive or dynamic support).
+        /// </summary>
+        /// <returns></returns>
+        public IDictionary<string, object> GetRawDictionary()
+        {
+            return _rawDictionary;
+        }
+
+        /// <summary>
+        /// Returns the prased model.
+        /// </summary>
+        public TModel GetModel()
+        {
+            return _model;
+        }
+
+        /// <summary>
+        /// Allow explicit casts into the model type.
+        /// </summary>
+        /// <param name="model"></param>
+        public static explicit operator TModel(InputModelDictionary<TModel> input)
+        {
+            return input._model;
+        }
+    }
+}
---
+++ Kirei.Repositories.GraphQL/InputModelDictionary/InputModelDictionaryObjectGraphType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQL.Types;
+
+namespace Kirei.Repositories.GraphQL
+{
+    /// <summary>
+    /// InputObjectGraphType subclass that overrides ParseDictionary to return a InputModelDictionary object rather than just the
+    /// parsed model so we can preserve the dictionary as it was received as well as the parsed result.
+    /// 
+    /// This is useful when you want to only update changed fields in a model in a database for example.
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public class InputModelDictionaryObjectGraphType<TModel> : InputObjectGraphType<TModel>
+    {
+        public InputModelDictionaryObjectGraphType()
+            :base()
+        {
+        }
+
+        /// <summary>
+        /// Override the parsing process to preserve both the parsed model (standard behaviour) and the dictionary/changes actually received from the mutation.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public override object ParseDictionary(IDictionary<string, object> value)
+        {
+            var model = (TModel)base.ParseDictionary(value);
+            return new InputModelDictionary<TModel>(model, value);
+        }
+    }
+}
